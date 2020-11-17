79911406b327f8ebd917d1ea00d039f51148ebf1
Winnie Yap <winnieyap20@gmail.com>
2020-11-18 01:57:02 +0800

    left some logic problem

--- app/src/main/java/com/example/sanitizersensor/MainActivity.kt
+++ app/src/main/java/com/example/sanitizersensor/MainActivity.kt
@@ -7,6 +7,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Vibrator
 import android.widget.TextView
@@ -18,11 +19,13 @@ class MainActivity: AppCompatActivity(), SensorEventListener {
     val secondary = FirebaseDatabase.getInstance("https://bait2123-202010-03.firebaseio.com/")
     val lcdbkG = secondary.getReference("PI_03_CONTROL").child("lcdbkG");
     val lcdbkR = secondary.getReference("PI_03_CONTROL").child("lcdbkR");
+    val lcdbkB = secondary.getReference("PI_03_CONTROL").child("lcdbkB");
 
     //primary firebase
     val primary: FirebaseDatabase = FirebaseDatabase.getInstance("https://solenoid-lock-f65e8.firebaseio.com/")
     val room: DatabaseReference = primary.getReference("Room")
-    val sanitizer = primary.getReference("Room").child("Room1").child("SanitizerLeft")
+    val sanitizer = primary.getReference("Room").child("Room1").child("sanitizerLeft")
+    val sanitizerUsed = primary.getReference("Room").child("Room1").child("sanitizerUsed")
 
     //variables
     var textview: TextView? = null
@@ -33,6 +36,10 @@ class MainActivity: AppCompatActivity(), SensorEventListener {
     var vibrator: Vibrator? = null
     var Sanitizer: Int ?= null
     var limitSanitize: Int ?= null
+    var userSanitize:Int ?=null
+    var finish:MediaPlayer ?=null
+    var limit:MediaPlayer ?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,14 +49,25 @@ class MainActivity: AppCompatActivity(), SensorEventListener {
         textview = findViewById<TextView>(R.id.textView)
         sanLeft = findViewById<TextView>(R.id.sanLeft)
 
+        //music to be play
+        finish = MediaPlayer.create(this, R.raw.finish);
+        limit = MediaPlayer.create(this, R.raw.limit);
+
+        //initialise database
+        lcdbkG.setValue("0")
+        lcdbkR.setValue("0")
+        lcdbkB.setValue("0")
+
         //read sanitizer available from firebase
         room.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 //storing the amount into code variable
-                var totalSanitizer = dataSnapshot.child("Room1").child("SanitizerLeft").value.toString().toInt();
+                var totalSanitizer = dataSnapshot.child("Room1").child("sanitizerLeft").value.toString().toInt();
                 var limitSanitizer = dataSnapshot.child("Room1").child("noOfPax").value.toString().toInt();
+                var usedSanitizer  = dataSnapshot.child("Room1").child("sanitizerUsed").value.toString().toInt();
                 Sanitizer = totalSanitizer
                 limitSanitize = limitSanitizer
+                userSanitize = usedSanitizer
                 sanLeft!!.text = "$Sanitizer drops sanitizer left"
 
             }
@@ -75,39 +93,56 @@ class MainActivity: AppCompatActivity(), SensorEventListener {
 
         //when a hand is near
         if(sensorEvent.values[0].toDouble() == 0.0){
+            userSanitize = userSanitize?.plus(1)
+
             //when sanitizer still available
             if(Sanitizer!! > 0){
 
-
-
-                //update sanitizer left
-                Sanitizer = Sanitizer!! -1
-                //write to UI
-                sanLeft!!.text = "$Sanitizer drops sanitizer left"
-                textview!!.text = "Ps! Here's your sanitizer"
-                //write reaction when sanitizer dispense to common resources database : Green Light
-                lcdbkG.setValue("255")
-                lcdbkR.setValue("0")
-                //write to resources left firebase
-                sanitizer.setValue(Sanitizer.toString())
-
-
-                //to make a auto update value scene
-                if(Sanitizer == 0){
-                    Thread.sleep(5_000)
-                    //no more sanitizer, set to Red Light
-                    lcdbkR.setValue("255")
-                    //send message to worker to refill sanitizer
-                    //debugging
+                //check if the user use more than limit
+                //if yes: play limit()
+                if(userSanitize!! > this!!.limitSanitize!!){
+                    playlimit()
                 }
+
+                //if no: continue to check stock ability
+                else{
+                    //update sanitizer left
+                    Sanitizer = Sanitizer!! -1
+                    //write to UI
+                    textview!!.text = "Ps! Here's your sanitizer"
+                    sanLeft!!.text = "$Sanitizer drops sanitizer left"
+                    //write reaction when sanitizer dispense to common resources database : Green Light
+                    lcdbkG.setValue("255")
+                    lcdbkR.setValue("0")
+                    //write data to firebase
+                    sanitizer.setValue(Sanitizer.toString())
+                    sanitizerUsed.setValue(userSanitize.toString())
+                }
+                if(Sanitizer ==0){
+                    Thread.sleep(1000L)
+                    //set to red light cause no more dy but no nee notify user unless they try to get
+                    lcdbkR.setValue("255")
+                }
+
+            }
+            else{
+                sanLeft!!.text = "No more sanitizer left"
+                playFinish()
             }
         }
         else{
-            sanLeft!!.text = "No more sanitizer left"
             textview!!.text = "No human detected."
             lcdbkG.setValue("0")
         }
 
+    }
+
+    private fun playFinish() {
+        finish?.start()
+    }
+
+    private fun playlimit() {
+        limit?.start();
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
